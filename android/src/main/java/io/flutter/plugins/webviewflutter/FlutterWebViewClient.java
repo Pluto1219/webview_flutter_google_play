4b32de3222077736f2d82191626211a65fc17b75
Juillet <1990218899@qq.com>
2021-04-15 13:04:47 +0800

    1、init

--- android/src/main/java/io/flutter/plugins/webviewflutter/FlutterWebViewClient.java
+++ android/src/main/java/io/flutter/plugins/webviewflutter/FlutterWebViewClient.java
@@ -6,7 +6,7 @@ package io.flutter.plugins.webviewflutter;
 
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
-import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.net.http.SslError;
@@ -214,26 +214,57 @@ class FlutterWebViewClient {
       }
 
       @Override
-      public void onReceivedSslError(WebView view, final SslErrorHandler handler, SslError error) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
-        builder.setMessage("SSL认证失败，是否继续访问？");
-        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+      public void onReceivedSslError(WebView view, final SslErrorHandler handler, SslError error)
+      {
+        handleSslError(view, handler, error);
+      }
+
+      private void handleSslError(WebView view, final SslErrorHandler handler, SslError error) {
+        Context context = view.getContext();
+        android.app.AlertDialog.Builder dialogBuilder = new android.app.AlertDialog.Builder(context);
+//        String message = context.getString(R.string.ssl_error_message_default);
+        String message = "SSL Certificate error.";
+        int errorType = error.getPrimaryError();
+        switch (errorType) {
+          case SslError.SSL_UNTRUSTED:
+//            message = context.getString(R.string.ssl_error_message_untrusted);
+            message = "The certificate authority is not trusted.";
+            break;
+          case SslError.SSL_EXPIRED:
+//            message = context.getString(R.string.ssl_error_message_expired);
+            message = "The certificate has expired.";
+            break;
+          case SslError.SSL_IDMISMATCH:
+//            message = context.getString(R.string.ssl_error_message_id_mismatch);
+            message = "The certificate Hostname mismatch.";
+            break;
+          case SslError.SSL_NOTYETVALID:
+//            message = context.getString(R.string.ssl_error_message_not_valid);
+            message = "The certificate is not yet valid.";
+            break;
+        }
+//        message += " " + context.getString(R.string.ssl_error_wether_continue);
+        message += " Do you want to continue anyway?";
+//        dialogBuilder.setTitle(R.string.ssl_error_title);
+        dialogBuilder.setTitle("SSL Certificate Error");
+        dialogBuilder.setMessage(message);
+
+//        dialogBuilder.setPositiveButton(R.string.string_continue, new DialogInterface.OnClickListener() {
+        dialogBuilder.setPositiveButton("continue", new DialogInterface.OnClickListener() {
           @Override
           public void onClick(DialogInterface dialog, int which) {
             handler.proceed();
           }
         });
-
-        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+//        dialogBuilder.setNegativeButton(R.string.string_cancel, new DialogInterface.OnClickListener() {
+        dialogBuilder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
           @Override
           public void onClick(DialogInterface dialog, int which) {
             handler.cancel();
           }
         });
-
-        AlertDialog dialog = builder.create();
+        android.app.AlertDialog dialog = dialogBuilder.create();
         dialog.show();
-
       }
     };
   }
