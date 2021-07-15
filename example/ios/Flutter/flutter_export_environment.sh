d6c247223984784f8e817e1950433cfe3be36ec5
Juillet <1990218899@qq.com>
2021-07-15 14:09:10 +0800

    1、2.0.10

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/Juillet/Juillet/work/DevTools/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/Juillet/Juillet/work/my/workspace/flutter/webview_flutter_google_play_old/example"
+export "FLUTTER_APPLICATION_PATH=/Users/Juillet/Juillet/work/my/workspace/flutter/webview_flutter_google_play/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
